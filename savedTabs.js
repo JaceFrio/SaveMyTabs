b5c599be13a03a15a13a420ebe37e8a86ca17f3a
JaceFrio <jbh.pwns@gmail.com>
2023-01-15 04:52:39 -0700

    adding font color radio button functionality

--- savedTabs.js
+++ savedTabs.js
@@ -1,3 +1,5 @@
+// TODO: add font color radio button functionality
+
 // regex for hex colors
 const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
 
